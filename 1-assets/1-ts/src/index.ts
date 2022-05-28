d518620025d4eb5fcc241a6a8f5ea87ffe180429
Alex Norris <drnorrisphd@gmail.com>
2022-05-28 18:12:14 -0400

    Added background color, began work on language selector

---
+++ 1-assets/1-ts/src/index.ts
@@ -0,0 +1,9 @@
+function toggleDescriptor(language) {
+    let description = document.getElementById("description");
+    if (language === "Korean") {
+        description.innerHTML = "Show Korean Text";
+    }
+    else {
+        description.innerHTML = "Show English Text";
+    }
+}
